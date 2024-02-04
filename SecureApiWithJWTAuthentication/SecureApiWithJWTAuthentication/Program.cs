dd7bf728a152e10ce131706fb1d1529d717db951
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-04 22:54:04 +0200

    replace default logger with serilog

--- SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/Program.cs
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/Program.cs
@@ -1,7 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SecureApiWithJWTAuthentication.Authentication;
 using SecureApiWithJWTAuthentication.DbContexts;
+using SecureApiWithJWTAuthentication.Models;
+using SecureApiWithJWTAuthentication.Services;
+using Serilog;
+using System.Text;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/jwtAuthApi.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,6 +26,17 @@ builder.Services.AddDbContext<AppDbContext>(
             builder.Configuration["ConnectionStrings:UsersApiDbConnection"])
 );
 
+builder.Services.AddScoped<IUserServices, UserServices>();
+builder.Services.AddSingleton(provider =>
+{
+    return new SigningConfiguration(builder.Configuration["JWT:Secret"]);
+
+});
+builder.Services.Configure<JwtConfiguration>(options => builder.Configuration.GetSection("JWT"));
+
+var jwtConfiguration = builder.Services.BuildServiceProvider().GetService<JwtConfiguration>();
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -21,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
