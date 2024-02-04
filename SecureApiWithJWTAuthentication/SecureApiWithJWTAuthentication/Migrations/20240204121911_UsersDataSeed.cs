e21bd66b313df87463e590a64bfdd896d59e9f91
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-04 15:20:21 +0200

    add initial and seeding migration

---
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/Migrations/20240204121911_UsersDataSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureApiWithJWTAuthentication.Migrations
+{
+    public partial class UsersDataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "Password", "UserName" },
+                values: new object[] { 1, "ayah", "shraim", "12345", "ayahShraim" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "Password", "UserName" },
+                values: new object[] { 2, "regular", "user", "11111", "regularUser" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
