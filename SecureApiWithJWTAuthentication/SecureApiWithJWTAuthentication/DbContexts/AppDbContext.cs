7cfc82e3a6adacd8601532882197d36d20370268
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-04 15:19:08 +0200

    add db context and extension method for seeding

---
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/DbContexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureApiWithJWTAuthentication.Entities;
+namespace SecureApiWithJWTAuthentication.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { 
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.SeedUsers();
+        }
+    }
+}
---
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/DbContexts/AppDbContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureApiWithJWTAuthentication.Entities;
+
+namespace SecureApiWithJWTAuthentication.DbContexts
+{
+    public static class AppDbContextExtensions
+    {
+        public static void SeedUsers(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, UserName = "ayahShraim", FirstName = "ayah", LastName = "shraim", Password = "12345" },
+                new User { Id = 2, UserName = "regularUser", FirstName = "regular", LastName = "user", Password = "11111" }
+            );
+        } 
+    }
+}
