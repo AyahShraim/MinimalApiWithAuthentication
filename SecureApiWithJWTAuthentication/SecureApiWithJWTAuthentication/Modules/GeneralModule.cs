277c50f9b86c6c72a0f736933d720d68bc8ff972
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-05 11:11:59 +0200

    add carter for cleaning enpoints

---
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/Modules/GeneralModule.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using System.Security.Claims;
+
+namespace SecureApiWithJWTAuthentication.Modules
+{
+    public class GeneralModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/welcome", (ClaimsPrincipal user) =>
+            {
+                var firstName = user.FindFirst(ClaimTypes.GivenName)?.Value;
+                var lastName = user.FindFirst(ClaimTypes.Surname)?.Value;
+                var message = $"Hey  {firstName} {lastName} ! Welcome to the Api :)";
+                return Results.Ok(message);
+            }).RequireAuthorization();
+
+            app.MapGet("/specialResource", (ClaimsPrincipal user) =>
+            {
+                return Results.Ok("This is a special resource for the first member in the system.");
+            }).RequireAuthorization("MustBeFirstMemberInSystem"); 
+        }
+    }
+}
---
+++ SecureApiWithJWTAuthentication/SecureApiWithJWTAuthentication/Modules/UserModule.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using SecureApiWithJWTAuthentication.Authentication;
+using SecureApiWithJWTAuthentication.Models;
+
+namespace SecureApiWithJWTAuthentication.Modules
+{
+    public class UserModule : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+
+            app.MapPost("/Login", async (IJwtTokenService tokenService, AuthenticationCredentials authenticationCredentials) =>
+            {
+                var token = await tokenService.GenerateToken(authenticationCredentials);
+                if (token == null)
+                {
+                    return Results.Unauthorized();
+                }
+                return Results.Ok(token);
+            });
+        }
+    }
+}
